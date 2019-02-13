06eae49b05d5a97d06d47da4276b634a59b5b495
George Higbie <ghigbie@monigle.com>
2019-02-13 08:48:28 -0700

    Create associative array

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Moo</h1>
+<?php
 
-</body>
-</html>
+define("PAGE_TITLE", "Todo List Written in PHP");
+$todos = ['Walk the dog', 'Hunt the rabbit', 'Show the cat who is boss'];
+$animals = [
+    'type' => 'dog',
+    'age' => 4,
+    'fur_color' => 'brown'
+];
+require 'index.view.php';
+
+
+
+
+
--- index.view.php
+++ index.view.php
@@ -1,7 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1><?= PAGE_TITLE_ALTERNATE ?></h1>
+<ul>
+    <?php foreach($todos as $todo): ?>
+        <li><?= $todo ?></li>
+    <?php endforeach; ?>
+</ul>
+<br>
+<ul>
+    <?php foreach($todos as $todo) {
+        echo "<li>$todo</li>";
+    }?>
+</ul>
+<ul>
+    <?php foreach($animals as $key => $value):?>
+        <li>
+            <strong><?=$key?>: </strong>
+            <?= $value ?>
+        </li>
+    <?php endforeach; ?>
 
-define("PAGE_TITLE", "Todo List Written in PHP");
-$todos = ['Walk the dog', 'Hunt the rabbit', 'Show the cat who is boss'];
-
-require 'index.view.php';
-
+</ul>
+</body>
+</html>
